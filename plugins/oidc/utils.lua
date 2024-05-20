052ffd8773931b918693b219a83af0cc2ec409f8
Gökhan Nihal <gokhan@bestcloudfor.me>
2024-05-20 23:28:56 +0300

    update values yaml

---
+++ plugins/oidc/utils.lua
@@ -0,0 +1,134 @@
+local cjson = require("cjson")
+local cjson_safe = require "cjson.safe"
+
+local M = {}
+
+local function parseFilters(csvFilters)
+  local filters = {}
+  if (not (csvFilters == nil)) then
+    for pattern in string.gmatch(csvFilters, "[^,]+") do
+      table.insert(filters, pattern)
+    end
+  end
+  return filters
+end
+
+function M.get_redirect_uri_path(ngx)
+  local function drop_query()
+    local uri = ngx.var.request_uri
+    local x = uri:find("?")
+    if x then
+      return uri:sub(1, x - 1)
+    else
+      return uri
+    end
+  end
+
+  local function tackle_slash(path)
+    local args = ngx.req.get_uri_args()
+    if args and args.code then
+      return path
+    elseif path == "/" then
+      return "/cb"
+    elseif path:sub(-1) == "/" then
+      return path:sub(1, -2)
+    else
+      return path .. "/"
+    end
+  end
+
+  return tackle_slash(drop_query())
+end
+
+function M.get_options(config, ngx)
+  return {
+    client_id = config.client_id,
+    client_secret = config.client_secret,
+    discovery = config.discovery,
+    introspection_endpoint = config.introspection_endpoint,
+    timeout = config.timeout,
+    introspection_endpoint_auth_method = config.introspection_endpoint_auth_method,
+    bearer_only = config.bearer_only,
+    realm = config.realm,
+    redirect_uri_path = config.redirect_uri_path or M.get_redirect_uri_path(ngx),
+    scope = config.scope,
+    response_type = config.response_type,
+    ssl_verify = config.ssl_verify,
+    token_endpoint_auth_method = config.token_endpoint_auth_method,
+    recovery_page_path = config.recovery_page_path,
+    filters = parseFilters(config.filters),
+    logout_path = config.logout_path,
+    redirect_after_logout_uri = config.redirect_after_logout_uri,
+    scopes_required = config.scopes_required,
+    scopes_claim = config.scopes_claim
+  }
+end
+
+function M.exit(httpStatusCode, message, ngxCode)
+  ngx.status = httpStatusCode
+  ngx.say(message)
+  ngx.exit(ngxCode)
+end
+
+function M.injectAccessToken(accessToken)
+  ngx.req.set_header("X-Access-Token", accessToken)
+end
+
+function M.injectIDToken(idToken)
+  local tokenStr = cjson.encode(idToken)
+  ngx.req.set_header("X-ID-Token", ngx.encode_base64(tokenStr))
+end
+
+function M.injectUser(user)
+  local tmp_user = user
+  tmp_user.id = user.sub
+  tmp_user.username = user.preferred_username
+  ngx.ctx.authenticated_credential = tmp_user
+  local userinfo = cjson.encode(user)
+  ngx.req.set_header("X-Userinfo", ngx.encode_base64(userinfo))
+end
+
+function M.has_bearer_access_token()
+  local header = ngx.req.get_headers()['Authorization']
+  if header and header:find(" ") then
+    local divider = header:find(' ')
+    if string.lower(header:sub(0, divider-1)) == string.lower("Bearer") then
+      return true
+    end
+  end
+  return false
+end
+
+-- Convert a lua table into a lua syntactically correct string
+function M.table_to_string(tbl)
+    local result = cjson_safe.encode(tbl)
+    return result
+    --[[
+    if tbl == nil then return "{}" end 
+    local result = "{"
+    for k, v in pairs(tbl) do
+        -- Check the key type (ignore any numerical keys - assume its an array)
+        if type(k) == "string" then
+            result = result.."[\""..k.."\"]".."="
+        end
+
+        -- Check the value type
+        if type(v) == "table" then
+            result = result..M.table_to_string(v)
+        elseif type(v) == "boolean" then
+            result = result..tostring(v)
+        else
+            result = result.."\""..v.."\""
+        end
+        result = result..","
+    end
+    -- Remove leading commas from the result
+    if result ~= "" then
+        result = result:sub(1, result:len()-1)
+    end
+    return result.."}"
+    --]]
+end
+
+
+return M
